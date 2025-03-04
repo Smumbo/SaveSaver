bf79138239e6f46c9b57d98d6c6ad1cfcd6319eb
Jonah Venglarcik <jonahvenglarcik@gmail.com>
2025-03-03 18:55:48 -0700

    Add some error handling for files

--- src/SaveSaver.java
+++ src/SaveSaver.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class SaveSaver {
     private static final String USAGE_STRING = """
-    usage: java -jar SaveSaver.jar <savePath> [(-u | -d) <cloudPath>] [-b <backupPath> <backupNumber>...]
+    usage: java -jar SaveSaver.jar <savePath> [(-u | -d) <cloudPath>] [-b <backupPath> <backupCount>...]
 
     arguments:
     \t<savePath>\t\tpath to the game save folder
@@ -18,7 +18,7 @@ public class SaveSaver {
 
     \t-b, --backup\t\tcreate a backup of the save folder
     \t    <backupPath>\twhere to save the backup to
-    \t    <backupNumber>\tnumber of rotating backups to keep
+    \t    <backupNumber>\tnumber of rotating backups to keep at path
     """;
     private static final List<String> UPLOAD_OPTIONS = List.of("-u", "--upload");
     private static final List<String> DOWNLOAD_OPTIONS = List.of("-d", "--download");
@@ -32,7 +32,13 @@ public class SaveSaver {
 
     private static void parse(String[] args) {
         System.out.println(Arrays.toString(args));
+
+        // Save path
         savePath = new File(args[0]);
+        if (!savePath.isDirectory()) {
+            System.err.println(String.format("Invalid save path (%s), save path must be a directory", args[0]));
+            System.exit(1);
+        }
 
         for (int i = 1; i < args.length; i++) {
             String token = args[i];
@@ -54,16 +60,24 @@ public class SaveSaver {
                     System.exit(1);
                 }
                 cloudPath = new File(args[i + 1]);
+                if (!cloudPath.isDirectory()) {
+                    System.err.println(String.format("Invalid cloud path (%s), cloud path must be a directory", args[i + 1]));
+                    System.exit(1);
+                }
                 i++;
             }
 
             // Backups
             if (BACKUP_OPTIONS.contains(token)) {
                 if (i + 2 >= args.length) {
-                    System.err.println("Missing backup path or number");
+                    System.err.println("Missing backup arguments");
                     System.exit(1);
                 }
                 File backupPath = new File(args[i + 1]);
+                if (!backupPath.isDirectory()) {
+                    System.err.println(String.format("Invalid backup path (%s), backup path must be a directory", args[i + 1]));
+                    System.exit(1);
+                }
                 int backupNumber = Integer.parseInt(args[i + 2]);
                 backups.add(new Backup(backupPath, backupNumber));
                 i += 2;
