a11b1ca0067c8f00f87733d1a8a8a25e0a3dbdf0
Jonah Venglarcik <jonahvenglarcik@gmail.com>
2025-03-03 18:46:09 -0700

    Move Backup class to new file

---
+++ src/Backup.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+public class Backup {
+    public File path;
+    public int number;
+
+    public Backup(File path, int number) {
+        this.path = path;
+        this.number = number;
+    }
+}
